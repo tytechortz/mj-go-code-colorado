2a4974354fc7832abb3f272fe3faf15aaf127700
James Swank <jmswank7@gmail.com>
2021-04-06 09:50:26 -0600

    app running

--- application.py
+++ application.py
@@ -1,1 +1,26 @@
 import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+from homepage import Homepage
+
+
+app = dash.Dash()
+application = app.server
+
+app.layout = html.Div([
+    dcc.Location(id = 'url', refresh = False),
+    html.Div(id = 'page-content')
+])
+
+@app.callback(Output('page-content', 'children'),
+            [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/revenue':
+        return revenue_App()
+    else:
+        return Homepage()
+
+
+if __name__ == '__main__':
+    application.run(debug=True, port=8000)
